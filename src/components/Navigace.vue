d9cf9575aa359b0a94ac547b72ad9342b45aa77e
Jana Novotná <jana.posedelova@gmail.com>
2020-08-07 12:17:09 +0200

    Přidání náhledů produktů na hlavní stránce

--- src/components/Navigace.vue
+++ src/components/Navigace.vue
@@ -1,16 +1,20 @@
 <template>
-  <div>
-    <router-link to="/"
-      ><img src="../assets/Woolpower-logo.png" alt="logo"
-    /></router-link>
-    <router-link to="/rada_Lite">Řada Lite</router-link>
-    <router-link to="/rada_Ullfrotte">Řada Ullfrotté</router-link>
-    <router-link to="/detske_obleceni">Dětské oblečení</router-link>
-    <router-link to="/doplnky">Doplňky</router-link>
-    <router-link to="/pracovni_obleceni">Pracovní oblečení</router-link>
-    <router-link to="/Woolpowered">Woolpowered</router-link>
-    <router-link to="/o_Woolpoweru">O Woolpoweru</router-link>
-    <router-link to="/kontakt">Kontakt</router-link>
+  <div class="nav">
+    <div class="logo">
+      <router-link to="/"
+        ><img src="../assets/Woolpower-logo.png" alt="logo"
+      /></router-link>
+    </div>
+    <div class="nabidka">
+      <router-link to="/rada_Lite">Řada Lite</router-link>
+      <router-link to="/rada_Ullfrotte">Řada Ullfrotté</router-link>
+      <router-link to="/detske_obleceni">Dětské oblečení</router-link>
+      <router-link to="/doplnky">Doplňky</router-link>
+      <router-link to="/pracovni_obleceni">Pracovní oblečení</router-link>
+      <router-link to="/Woolpowered">Woolpowered</router-link>
+      <router-link to="/o_Woolpoweru">O Woolpoweru</router-link>
+      <router-link to="/kontakt">Kontakt</router-link>
+    </div>
   </div>
 </template>
 <script>
@@ -18,4 +22,17 @@ export default {
   name: "Menu",
 };
 </script>
-<style scoped></style>
+<style scoped>
+.nav {
+  display: flex;
+  flex-direction: column;
+}
+
+@media (min-width: 640px) {
+  .nav {
+    flex-direction: row;
+  }
+}
+@media (min-width: 1280px) {
+}
+</style>
--- src/components/Woolpower.vue
+++ src/components/Woolpower.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <H1>Zkouška</H1>
     <agile>
       <div>
         <img src="src/assets/Riverside2.jpg" alt="" class="slide" />
@@ -11,10 +10,25 @@
         <h3>slide 1</h3>
       </div>
       <div>
-        <img src="src/assets/Hiking1.jpg" alt="" class="slide" />
+        <img src="src/assets/Hiking2.jpg" alt="" class="slide" />
         <h3>slide n</h3>
       </div>
     </agile>
+    <div>
+      <h2>Woopower produkty</h2>
+    </div>
+    <div class="nabidka">
+      <div class="zakladni-vrstva"><h5>Ullfrotté</h5></div>
+
+      <div class="sloupec">
+        <div class="rada-lite nahledy"><h5>Lite</h5></div>
+        <div class="detske-obleceni nahledy"><h5>Pro děti</h5></div>
+      </div>
+      <div class="sloupec">
+        <div class="pracovni-obleceni nahledy"><h5>Pracovní oblečení</h5></div>
+        <div class="doplnky nahledy"><h5>Doplňky</h5></div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -28,6 +42,7 @@ export default {
 };
 </script>
 <style scoped>
+/* Carousel */
 agile nav-button {
   background: transparent;
   border: none;
@@ -48,8 +63,118 @@ agile nav-button:hover {
 
 .slide {
   display: block;
-  height: 500px;
+  height: 60vh;
   object-fit: cover;
   width: 100%;
+}
+/* nabídka produktů */
+h2 {
+  text-align: center;
+}
+
+.nabidka {
+  display: flex;
+  flex-direction: column;
+}
+
+.zakladni-vrstva {
+  display: flex;
+  background-image: url("../assets/HikingSummer2.jpg");
+  background-position: center center;
+  background-size: cover;
+  min-height: 300px;
+  padding: 10px;
+  margin: 5px;
+}
+
+.sloupec {
+  display: flex;
+  flex-direction: column;
+}
+
+.rada-lite {
+  background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+    url("../assets/RunningInOstersund2.jpg");
+  background-position: center top;
+  background-size: cover;
+  min-height: 200px;
+  margin: 5px;
+}
+
+.detske-obleceni {
+  background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+    url("../assets/AutumnKids.jpg");
+  background-position: center center;
+  background-size: cover;
+  min-height: 200px;
+  margin: 5px;
+}
+
+.pracovni-obleceni {
+  background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+    url("../assets/Work.jpg");
+  background-position: center top;
+  background-size: cover;
+  min-height: 200px;
+  margin: 5px;
+}
+
+.doplnky {
+  background: linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)),
+    url("../assets/Socks.jpg");
+  background-position: center center;
+  background-size: cover;
+  min-height: 200px;
+  margin: 5px;
+}
+
+h5 {
+  color: #fff;
+  font-size: 16px;
+  margin-bottom: 0;
+}
+
+.nahledy {
+  display: flex;
+
+  align-items: flex-end;
+  padding-left: 10px;
+}
+
+@media (min-width: 640px) {
+  h5 {
+    font-size: 18px;
+  }
+
+  .nabidka {
+    flex-direction: row;
+    flex-wrap: wrap;
+  }
+  .zakladni-vrstva {
+    flex: 1 0 100%;
+  }
+  .sloupec {
+    flex: 1 0 50%;
+  }
+}
+
+@media (min-width: 1024px) {
+  h5 {
+    font-size: 22px;
+  }
+
+  .nabidka {
+    flex-direction: row;
+    flex-wrap: nowrap;
+  }
+  .zakladni-vrstva {
+    flex: 2 0 auto;
+  }
+  .sloupec {
+    flex: 1 0 auto;
+  }
+}
+
+@media (min-width: 1280px) {
 }
 </style>

a76f75f841777c48bcd5b1d73d805998c54edc1a
Jana Novotná <jana.posedelova@gmail.com>
2020-08-09 00:24:15 +0200

    vytvořen první js soubor s daty produktů, stáhnuté fotky k tomuto souboru.

--- src/components/RadaLite.vue
+++ src/components/RadaLite.vue
@@ -1,4 +1,12 @@
-<template></template>
+<template>
+  <div>
+    <img src="src/assets/ActiveFamilyWinter.jpg" alt="Rodina na výletě na běžkách" />
+    <div>
+      <h1>Woolpower Lite</h1>
+      <p>Řada produktů Lite je vyráběna z jiného materiálu než ostatní výrobky Woolpower. U této řady nenajdeme tipická očka Ulfrotté na vnitřní straně. Řida Lite je určena do teplešjích podmínek nebo na náročnější aktivity v přírodě po celý rok jako jsou kolo, běh či běžky.</p>
+    </div>
+  </div>
+</template>
 <script>
 export default {};
 </script>

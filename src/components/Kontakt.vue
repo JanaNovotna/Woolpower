da9a89a5f49709ed3624c66b85c3e2b1194ed651
Jana Novotná <jana.posedelova@gmail.com>
2021-01-26 17:26:39 +0100

    Pokračování úprav v kontaktech, přidání českého ráje

--- src/components/Kontakt.vue
+++ src/components/Kontakt.vue
@@ -7,8 +7,9 @@
         <p class="odkaz">
           <a href="https://www.parys.cz" target="blank">www.parys.cz</a>
         </p>
+        <h3>Prodejna:</h3>
         <p>
-          Průmyslová 1292<br />50601 Jičín <br />
+          Průmyslová 1292, Jičín <br />
           <b>telefon:</b> 774 411 193 <br />
           <b>e-mail: </b><a href="mailto:info@parys.cz">info@parys.cz</a>
         </p>
@@ -20,41 +21,57 @@
             >www.fishhunt.cz</a
           >
         </p>
+        <h3>Prodejna:</h3>
         <p>
-          Kolínská 94<br />280 02 Nová Ves I <br />
+          Kolínská 94, Nová Ves I <br />
           <b>telefon:</b> 774 377 133 <br />
           <b>e-mail: </b><a href="mailto:shop@fishhunt.cz">shop@fishhunt.cz</a>
         </p>
       </div>
       <div class="prodejce">
-        <h2>Bushcraft</h2>
+        <h2>JüBo Bushcraft</h2>
         <p class="odkaz">
           <a href="https://www.bushcraftshop.cz" target="blank"
             >www.bushcraftshop.cz</a
           >
         </p>
+        <h3>Prodejny:</h3>
         <p>
-          Jesenická 22<br />787 01 Šumperk<br />
-          <b>telefon:</b> 605 011 644
-        </p>
-        <p>
-          Na Václavce 28<br />150 00 Praha 5<br />
+          Jesenická 22, Šumperk<br />
+          <b>telefon:</b> 605 011 644 <br />
+          <br />
+          Na Václavce 28, Praha 5<br />
           <b>telefon:</b> 737 730 056 <br />
           <b>e-mail: </b
           ><a href="mailto:info@bushcraftshop.cz">info@bushcraftshop.cz</a>
         </p>
       </div>
       <div class="prodejce">
-        <h2>Hanibal sport</h2>
+        <h2>Hanibal</h2>
         <p class="odkaz">
           <a href="https://www.hanibal.cz" target="blank">www.hanibal.cz</a>
         </p>
+        <h3>Prodejny:</h3>
         <p>
-          Korunní 16<br />120 00 Praha 2<br />
-          Senovážné náměstí 20<br />110 00 Praha 1<br />
-          Orlí 20<br />602 00 Brno<br />
+          Korunní 16, Praha 2 <br />
+          Senovážné náměstí 20, Praha 1<br />
+          Orlí 20, Brno<br />
           <b>telefon:</b> 220 920 820 <br />
           <b>e-mail: </b><a href="mailto:info@hanibal.cz">info@hanibal.cz</a>
+        </p>
+      </div>
+      <div class="prodejce">
+        <h2>Český ráj outdoor</h2>
+        <p class="odkaz">
+          <a href="https://www.ceskyraj.com" target="blank">www.ceskyraj.com</a>
+        </p>
+        <h3>Prodejna:</h3>
+        <p>
+          Staroměstské náměstí 20/21<br />
+          Mladá Boleslav<br />
+          <b>telefon:</b> 326 211 050 <br />
+          <b>e-mail: </b
+          ><a href="mailto:ceskyraj@ceskyraj.com">ceskyraj@ceskyraj.com</a>
         </p>
       </div>
     </div>
@@ -100,6 +117,14 @@ h2 {
   text-align: center;
 }
 
+h3 {
+  margin-bottom: 0;
+}
+
+hr {
+  border-top: 1px dashed darkgray;
+}
+
 .odkaz {
   font-size: 0.8rem;
   text-align: center;
@@ -118,8 +143,9 @@ h2 {
   justify-content: flex-start;
   margin: 0.5rem;
   padding: 1rem;
-  border: 1px solid rgb(165, 164, 164);
+  border: 1px solid rgb(241, 241, 241);
   border-radius: 5px;
+  box-shadow: 2px 2px 3px rgb(165, 164, 164);
 }
 
 .kontakt {
